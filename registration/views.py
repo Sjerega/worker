56164fa30772133c37f48ef1555c0ff9690f874d
srg lkh <sjerega@yandex.ru>
2019-08-21 15:27:45 +0300

    create templates, change models

--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .models import Category
+from .models import Category, User, Job, Photo
 
 
 class IndexView(View):
     template_name = 'index.html'
 
     def get(self, request):
-        context = {'categories': Category.objects.all()}
+        context = {'categories': Category.objects.all(), 'jobs': Job.objects.all()}
         return render(request, self.template_name, context=context)
+
+
+class CategoryView(View):
+    template_name = 'category.html'
+
+    def get(self, request, slug=None):
+#        jobs = Job.objects.filter(category=Category.objects.get(slug=slug)) if slug else Job.objects.all()
+        category = Category.objects.get(slug=slug)
+#        jobs = Job.objects.filter(category=category)
+        context = {
+            'categories': Category.objects.all(),
+            'category': category,
+            'jobs': Job.objects.filter(category=category)
+        }
+        return render(request, self.template_name, context=context)
+
+
+class WorkerView(View):
+    template_name = 'worker.html'
+
+    def get(self, request, id=None):
+        user = User.objects.get(id=id)
+        context = {
+            'categories': Category.objects.all(),
+            'worker': user,
+            'jobs': Job.objects.filter(user=user)
+        }
+        return render(request, self.template_name, context=context)
+
+
+class JobView(View):
+    template_name = 'job.html'
+
+    def get(self, request, id=None):
+        # if id:
+            context = {
+                'categories': Category.objects.all(),
+                'job': Job.objects.get(id=id),
+                'photos': Photo.objects.filter(job=id),
+            }
+            return render(request, self.template_name, context=context)
+        # else:
+        #     redirect
--- worker/urls.py
+++ worker/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
-from registration.views import IndexView
+#from django.views.generic.base import TemplateView
+from registration.views import IndexView, CategoryView, WorkerView, JobView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view())
+    path('', IndexView.as_view()),
+    path('category/<str:slug>/', CategoryView.as_view()),
+    path('job/<int:id>/', JobView.as_view()),
+    path('worker/<int:id>/', WorkerView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
